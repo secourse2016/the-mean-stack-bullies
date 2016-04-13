c8804557f322738427fcc7141b4806679ae411cd
mohamedAbouzeid <mohamed.amr-abouzeid@student.guc.edu.eg>
2016-04-13 20:49:21 +0200

    to be Seeded only if the collection does not exist

--- db.js
+++ db.js
@@ -24,15 +24,30 @@ module.exports = {
 		cb();
 	},
 	seed:function(model,entities) {  
-    var promise = new mongoose.Promise;
-    model.create(entities, function(err) {
-        if(err) { promise.reject(err); }
-        else    { promise.resolve();
-                  console.log("seeding completed");
-                   }
-    });
-    return promise;
-	}
+	    model.count( {}, function(err, count) {
+	          if(count==0){
+	          	var promise = new mongoose.Promise;
+	    		model.create(entities, function(err) {
+	      			if(err) 
+	      			{ 
+	      				promise.reject(err); 
+	      			}
+	      		  	else{ 
+	        			promise.resolve();
+	      	            console.log("seeded correctly");
+	           		 }
+	   			});
+	    		return promise;
+				}
+				else
+				{
+					console.log("already seeded");
+				}
+	    		
+	    });
+
+    }
+    
 
 };
 
