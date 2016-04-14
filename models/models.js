d6c601aa52566a12f3b61711cd966ba928be11dd
HatemMorgan <hatemmorgan17@gmail.com>
2016-04-14 21:14:31 +0200

    seedOutFlights function to seed outFlight collection is added

--- models/models.js
+++ models/models.js
@@ -3,9 +3,10 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 var Db = require('../db.js');
 
-// Db.init(function(){
-// console.log("here");
-// });
+
+Db.init(function(){
+console.log("here");
+});
 
 // Db.seed();
 
@@ -97,9 +98,68 @@ mongoose.model('inFlight', ingoingflightSchema);
 
 
 
-exports.seedingFunction=function(cb){
-    Db.seed(mongoose.model('Airport'),require('../airports.json'),function(){
-        Db.seed(mongoose.model('outFlight'),require('../outflights.json'),function(){
+
+function seedOutFlights(){
+  var newOutFlights = [];
+   
+ 
+  console.log(d);
+  for(var j=0 ; j<48;j++)
+  {
+  
+
+ var outflights = require('../outflights');
+
+ 
+
+       for(var i=0 ; i<outflights.length ;i++){
+    // Day flights
+       var d =new Date();
+     d.setDate(d.getDate() + j);    
+
+    var x = new Date();
+     x.setDate(x.getDate() + j);
+    newOutFlights[i + (j * outflights.length)]= JSON.parse(JSON.stringify(outflights[i]));
+      
+      
+    var hours= 0;
+    if(i<20){
+         hours = Math.random() * 12;
+     
+    }else{
+        hours = (Math.random() * 12) + 12;
+    }  
+       d.setHours(hours); 
+
+        newOutFlights[i + (j * outflights.length)].departureDateTime= d;
+        x.setHours(hours + (Math.random()* 8) + 2); 
+
+     newOutFlights[i + (j * outflights.length)].arrivalDateTime= x ;
+    }
+  }
+  return newOutFlights;
+ // console.log(newOutFlights[0]);
+ // console.log(newOutFlights[30]);
+ // console.log(newOutFlights[1879]);
+ // console.log(newOutFlights[1880]);
+}
+
+
+seedingFunction=function(cb){
+Db.seed(mongoose.model('Airport'),require('../airports.json'),function(){
+    mongoose.model('outFlight').count( {}, function(err, count) { 
+          if(count !=0){
+            console.log("already seeded");
+          }else{
+             mongoose.model('outFlight').create(seedOutFlights(), function (err) {
+                      if (err) {
+                        console.log("cannot seed outFlight");
+                    }else{
+                        console.log("outFlight seeded");
+                    }
+               });     
+          }     
+      
             Db.seed(mongoose.model('inFlight'),require('../returnflights.json'),function(){
                 Db.seed(mongoose.model('Reservation'),require('../reservations.json'),function(){
                     Db.seed(mongoose.model('Booking'),require('../bookings.json'),function(){
@@ -115,3 +175,6 @@ exports.seedingFunction=function(cb){
 
 };
 
+seedingFunction(function(){
+    console.log("seeding database");
+});
