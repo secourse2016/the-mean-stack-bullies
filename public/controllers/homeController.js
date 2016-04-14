21dc32f3e5ce429a9aeb1d25a8d98f4d26198982
mohamedAbouzeid <mohamed.amr-abouzeid@student.guc.edu.eg>
2016-04-14 07:53:14 +0200

    separating the home Controller into many Ctrls (adding 2 other controllers)

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -51,58 +51,7 @@ app.directive('scrollOnClick', function() {
   }]);
 
 
-app.controller('myCtrl', function($scope) {
-  $scope.showMe=false;
-  $scope.showPay=false;
-  $scope.showTag=false;
-  
-  
-  $scope.click1= function()
-  {
-  	$scope.showMe=true;
-    $scope.include="../partials/bookingForm.html"
-  }
-   $scope.click2= function()
-  {
-  	$scope.showMe=true;
-    $scope.include="../partials/booking2.html"
 
-  }
-  $scope.click3=function()
-  {
-  	$scope.showMe=true;
-    $scope.include="../partials/checkreservation.html"
-
-  }
-
-
-   $scope.click4= function()
-  {
-    $scope.showMe=true;
-    $scope.showBooking=false;
-    $scope.showManage=false;
-    $scope.showBooking2=false;
-    $scope.showBooking3=true;
-    console.log("hiiiiiiiiiiiiiitag");
-
-  }
-   $scope.click5= function()
-  {
-    $scope.showPay=true;
-   
-    
-  }
-  $scope.tag=function()
-  {
-    $scope.showTag=true;
-
-  }
-    
-   $scope.close= function()
-  {
-  	$scope.showMe=false;
-  }
-});
 //End handling section
 /**
  * Main Controller
@@ -113,69 +62,6 @@ app.controller('myCtrl', function($scope) {
 
 
 
-app.controller('reservationsController', function($scope,cancelationReservation)
-{
-	$scope.searchReservations = function()
-	{
-	   // $http.get("./reservation.json")
-    //   .then(function(response) {
-      	   reservations =  cancelationReservation.getReservation();
-      	   found = false;
-           choosenReservation = 0;
-      	   for ( i = 0;i<reservations.length;i++)
-      	   {
-
-      	   	 if (reservations[i].bookingRefNumber == $scope.enteredReferenceNum)
-      	   	 {
-              choosenReservation = i;
-        
-          	if (reservations[i].confirmed)
-   	 	    		$scope.status = "Status : Confirmed";
-   	 	    	else
-   	 	    		$scope.status ="Status : Not Confirmed";
-   	 	    	found = true;
-                
-   	 	    	break;
-      
-      	   	 }
-           }
-           if (!found)
-           {
-          	 $scope.reservationExist = false;
-
-             $scope.names = ["Reference Number Not Found, Please check it and try again."];
-             $scope.flightDetails = [""];
-           }
-           else
-           {
-              $scope.result= "";
-              $scope.reservationExist = true;
-             //  $http.get("./flights.json")
-             // .then(function(flightsResponse) {
-              flights = cancelationReservation.getFlights();
-                for ( i = 0;i<flights.length;i++)
-                {
-             	    if (reservations[choosenReservation].flightNumber == flights[i].flightNumber.unique)
-                  {
-                        $scope.names = ["First Name : " + reservations[i].firstName
-                        ,"Last Name : " + reservations[i].lastName,
-                        "Passport number : " + reservations[i].passport_number,
-                       "Booking reference number : " + reservations[i].bookingRefNumber];
-
-                       $scope.flightDetails = ["From " + flights[i].originCity
-                       + " To " + flights[i].destinationCity ,"Date : " + flights[i].date,"Duration : " + flights[i].duration + " hours"] ;
-                       break; 
-                  }
-                }
-          //  });
-           }
-
-    //   });
-    
-	}
-
-
-});
 app.controller('validateCtrl', function($scope) {
     $scope.CardNumber= "";
     $scope.holderName = "";
