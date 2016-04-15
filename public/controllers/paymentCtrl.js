67ad39595914201633fb16a2228a6855e07eb5fc
Nad3in <nadinelsaeed@hotmail.com>
2016-04-15 19:12:32 +0200

    this page is done

--- public/controllers/paymentCtrl.js
+++ public/controllers/paymentCtrl.js
@@ -1,15 +1,73 @@
 app.controller('paymentCtrl', function($scope, $location,paySrv) {
-		$scope.clicktest=function() {
-     		var pa=[{
-     			visa:true,
-      			MasterCard: false,
-     			CardHolderName: "Aly Ameeeeeen",
-      			CardHolderNo: 13552354567467,
-     			Cvv: 321,
-      			ExpiryDate: "06 JAN 2017"
-     		}];
-     		paySrv.insertPayment(pa);
-     		console.log("kiko");
+     /**
+      * validating payment form
+      */
+      function paymentValidations(){
+        var isvalid =true;
+        var errMessage = "";
+         if($scope.holderN == null||!(/^[a-z ,.'-]+$/i.test($scope.holderN))){
+          errMessage+="please enter a valid card name \n";
+          isvalid = false;
+          }
+         if($scope.CardN == null||!(/^[0-9]{16}$/.test($scope.CardN))){
+           errMessage+="please enter a valid card number \n";
+           isvalid = false;
+          }
+         if(($scope.CVV == null)||!(/^[0-9]{3}$/.test($scope.CVV ))){
+           errMessage+="please enter a valid CVV \n";
+           isvalid =false;
+          }
+         if($scope.radioButton== null){
+           errMessage+="please choose either Visa or MasterCard \n";
+           isvalid = false;
+          } 
+
+         if($scope.expiryyear== null){
+           errMessage+="please choose expiry year \n";
+           isvalid =false;
+          } 
+
+         if($scope.expirymonth== null){
+           errMessage+="please choose choose expiry month \n";
+           isvalid =false;
+          }
+         if($scope.expiryday== null){
+           errMessage+="please choose choose expiry day \n";
+           isvalid =false;
+         }
+         if(isvalid == true){
+          errMessage = null;
+         }
+         return errMessage;
+       }
+
+       /**
+        * payment form submitting function
+        */
+		$scope.submitPaymentForm=function() {
+     		var errMessage = paymentValidations();
+        if(errMessage){
+          alert(errMessage);
+        }
+        else{
+          var boolea=false;
+          if($scope.radioButton=="visa"){
+            boolea=true;
+          }
+          else{
+            boolea=false;
+          }
+          var date =$scope.expiryday+" "+$scope.expirymonth+" "+$scope.expiryyear;
+          console.log(date);
+          var pa=[{
+       			visa:boolea,
+        		MasterCard: (!boolea),
+       			CardHolderName: $scope.holderN,
+        		CardHolderNo: $scope.CardN,
+       			Cvv: $scope.CVV,
+        		ExpiryDate: date
+       		}];
+       		paySrv.insertPayment(pa);
+    		}
   		}
-  		
 });
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,42 @@
 var express = require('express');
-var payControl= require('../serverController/paymentController.js');
+var paymentController= require('../serverController/paymentController.js');
+var paymentValidation= require('../Validations/paymentValidation.js');
 var router = express.Router();
 
-/* APIs */
+/*
+|==========================================================================
+| Payment Routes
+|==========================================================================
+|
+| These routes are related to the Payments.
+|
+*/
+    /**
+     * validating of payment middleware.
+     */
+    router.post('/api/insertpayment', function(req, res,next) {
+       
+      paymentValidation.validatePayment(req.body.payment[0],function(errmessage){
+         if(errmessage){
+          res.send(errmessage);
+         }else{
+
+          next();
+         }
+      });
+  
+  
+});
+
+    /**
+     * Inserting payment route.
+     */
 router.post('/api/insertpayment', function(req, res) {
-	//console.log(req.body.payment[0]);
-	payControl.addPaymentIntoDatabase(req.body.payment[0],function(){
-		res.send('payment added to the database');
-	});
+
+			paymentController.addPaymentIntoDatabase(req.body.payment[0],function(){
+				res.send('payment added to the database');
+			});
+	
   
 });
 
