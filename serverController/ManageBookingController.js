1b90a6d6a63345f689ab0e5c3b3450d80dd6effd
Alyameen <gindisgindis@hotmail.com>
2016-04-18 07:59:23 +0200

    Fixing the flight controller button

--- serverController/ManageBookingController.js
+++ serverController/ManageBookingController.js
@@ -7,24 +7,17 @@ exports.searchBookings =function(refNum,cb){
 
 
 	var resQuery = reservationModel.find();
-	resQuery.where('destination',flightData[0].destination);
-	resQuery.where('origin',flightData[0].origin);
+	resQuery.where('refNum',refNum);
+	
 
 	var bookingQuery = bookingModel.find();
-	
-	var x = new Date(flightData[0].departureDateTime);
-	x.setMinutes(0);
-	x.setHours(0);
-	var y = new Date(flightData[0].departureDateTime);
-	y.setMinutes(59);
-	y.setHours(23);
-	resQuery.where('departureDateTime',{"$gte":x
-		, "$lt": y});	
-
-	resQuery.exec(function (err, docs) {
+	bookingQuery.where('refNum',refNum);
 
 
-	cb(docs);
-});
+	resQuery.exec(function (err, resDocs) {
+		bookingQuery.exec(function(err2,bookDocs))
+
+		cb(resDocs,bookDocs);
+	});
 	
 }
